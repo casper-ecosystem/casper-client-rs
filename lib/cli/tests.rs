afedbd8dd6fb8cc734620a7b340c13fbb957e95e
Zach Showalter <zach@casperlabs.io>
2023-07-26 15:18:59 -0400

    tests for improved multisig support

--- lib/cli/tests.rs
+++ lib/cli/tests.rs
@@ -1,87 +1,106 @@
+use casper_types::DeployBuilderError::DeployMissingSessionAccount;
 use casper_types::{DeployExcessiveSizeError, ExecutableDeployItem, SecretKey};
 
+use crate::Error::DeployBuild;
 use crate::{Error, OutputKind, MAX_SERIALIZED_SIZE_OF_DEPLOY};
 
 use super::*;
 
+const SAMPLE_ACCOUNT: &str = "01722e1b3d31bef0ba832121bd2941aae6a246d0d05ac95aa16dd587cc5469871d";
 const PKG_HASH: &str = "09dcee4b212cfd53642ab323fbef07dafafc6f945a80a00147f62910a915c4e6";
 const ENTRYPOINT: &str = "entrypoint";
-const VERSION: &str = "0.1.0";
+const VERSION: &str = "2";
 const SAMPLE_DEPLOY: &str = r#"{
-      "hash": "4858bbd79ab7b825244c4e6959cbcd588a05608168ef36518bc6590937191d55",
-      "header": {
-        "account": "01f60bce2bb1059c41910eac1e7ee6c3ef4c8fcc63a901eb9603c1524cadfb0c18",
-        "timestamp": "2021-01-19T01:18:19.120Z",
-        "ttl": "10s",
-        "gas_price": 1,
-        "body_hash": "95f2f2358c4864f01f8b073ae6f5ae67baeaf7747fc0799d0078743c513bc1de",
-        "dependencies": [
-          "be5fdeea0240e999e376f8ecbce1bd4fd9336f58dae4a5842558a4da6ad35aa8",
-          "168d7ea9c88e76b3eef72759f2a7af24663cc871a469c7ba1387ca479e82fb41"
+  "hash": "1053f767f1734e3b5b31253ea680778ac53f134f7c24518bf2c4cbb204852617",
+  "header": {
+    "account": "01f60bce2bb1059c41910eac1e7ee6c3ef4c8fcc63a901eb9603c1524cadfb0c18",
+    "timestamp": "2022-12-11T18:37:06.901Z",
+    "ttl": "10s",
+    "gas_price": 1,
+    "body_hash": "0a80edb81389ead7fb3d6a783355d821313c8baa68718fa7478aa0ca6a6b3b59",
+    "dependencies": [],
+    "chain_name": "casper-test-chain-name-1"
+  },
+  "payment": {
+    "StoredVersionedContractByHash": {
+      "hash": "09dcee4b212cfd53642ab323fbef07dafafc6f945a80a00147f62910a915c4e6",
+      "version": 2,
+      "entry_point": "entrypoint",
+      "args": [
+        [
+          "name_01",
+          {
+            "cl_type": "Bool",
+            "bytes": "00",
+            "parsed": false
+          }
         ],
-        "chain_name": "casper-test-chain-name-1"
-      },
-      "payment": {
-        "StoredVersionedContractByHash": {
-          "hash": "09dcee4b212cfd53642ab323fbef07dafafc6f945a80a00147f62910a915c4e6",
-          "version": null,
-          "entry_point": "entrypoint",
-          "args": [
-            [
-              "name_01",
-              {
-                "cl_type": "Bool",
-                "bytes": "00",
-                "parsed": false
-              }
-            ],
-            [
-              "name_02",
-              {
-                "cl_type": "I32",
-                "bytes": "2a000000",
-                "parsed": 42
-              }
-            ]
-          ]
-        }
-      },
-      "session": {
-        "StoredVersionedContractByHash": {
-          "hash": "09dcee4b212cfd53642ab323fbef07dafafc6f945a80a00147f62910a915c4e6",
-          "version": null,
-          "entry_point": "entrypoint",
-          "args": [
-            [
-              "name_01",
-              {
-                "cl_type": "Bool",
-                "bytes": "00",
-                "parsed": false
-              }
-            ],
-            [
-              "name_02",
-              {
-                "cl_type": "I32",
-                "bytes": "2a000000",
-                "parsed": 42
-              }
-            ]
-          ]
-        }
-      },
-      "approvals": [
-        {
-          "signer": "01f60bce2bb1059c41910eac1e7ee6c3ef4c8fcc63a901eb9603c1524cadfb0c18",
-          "signature": "010f538ef188770cdbf608bc2d7aa9460108b419b2b629f5e0714204a7f29149809a1d52776b0c514e3320494fdf6f9e9747f06f2c14ddf6f924ce218148e2840a"
-        },
-        {
-          "signer": "01e67d6e56ae07eca98b07ecec8cfbe826b4d5bc51f3a86590c0882cdafbd72fcc",
-          "signature": "01c4f58d7f6145c1e4397efce766149cde5450cbe74991269161e5e1f30a397e6bc4c484f3c72a645cefd42c55cfde0294bfd91de55ca977798c3c8d2a7e43a40c"
-        }
+	[
+          "name_02",
+          {
+            "cl_type": "I32",
+            "bytes": "2a000000",
+            "parsed": 42
+          }
+        ]
       ]
-    }"#;
+    }
+  },
+  "session": {
+    "StoredVersionedContractByHash": {
+      "hash": "09dcee4b212cfd53642ab323fbef07dafafc6f945a80a00147f62910a915c4e6",
+      "version": 2,
+      "entry_point": "entrypoint",
+      "args": [
+        [
+          "name_01",
+          {
+            "cl_type": "Bool",
+            "bytes": "00",
+            "parsed": false
+          }
+        ],
+        [
+          "name_02",
+          {
+            "cl_type": "I32",
+            "bytes": "2a000000",
+            "parsed": 42
+          }
+        ]
+      ]
+ }
+  },
+  "approvals": [
+    {
+      "signer": "01f60bce2bb1059c41910eac1e7ee6c3ef4c8fcc63a901eb9603c1524cadfb0c18",
+      "signature": "01d701c27d7dc36b48fa457e4c7cc9999b444d7efb4a118c805b82d1f1af337437d00f9a9562694a7dd707abc01fa0158428a365a970853327d70d6d8f15aeea00"
+    },
+    {
+      "signer": "016e3725ffd940bddb56e692e6309c6c82d2def515421219ddfd1ea0952e52491a",
+      "signature": "010a973a45b72208b18da27b25ea62c6be31cd1b53b723b74cdd7e9f356d83df821b6431c973e2f6e24d10fdb213dc5e02d552ba113254e610992b6942ff76390e"
+    }
+  ]
+}"#;
+
+pub fn deploy_params_without_account() -> DeployStrParams<'static>{
+    DeployStrParams{
+        secret_key: "",
+        ttl:"10s",
+        chain_name: "casper-test-chain-name-1",
+        ..Default::default()
+    }
+}
+
+pub fn deploy_params_without_secret_key() -> DeployStrParams<'static> {
+    DeployStrParams {
+        secret_key: "",
+        ttl: "10s",
+        chain_name: "casper-test-chain-name-1",
+        session_account: SAMPLE_ACCOUNT,
+        ..Default::default()
+    }
+}
 
 pub fn deploy_params() -> DeployStrParams<'static> {
     DeployStrParams {
@@ -107,7 +126,8 @@ fn should_create_deploy() {
     let mut output = Vec::new();
 
     let deploy =
-        deploy::with_payment_and_session(deploy_params, payment_params, session_params).unwrap();
+        deploy::with_payment_and_session(deploy_params, payment_params, session_params, false)
+            .unwrap();
     crate::write_deploy(&deploy, &mut output).unwrap();
 
     // The test output can be used to generate data for SAMPLE_DEPLOY:
@@ -146,7 +166,7 @@ fn should_fail_to_create_large_deploy() {
         "",
     );
 
-    match deploy::with_payment_and_session(deploy_params, payment_params, session_params) {
+    match deploy::with_payment_and_session(deploy_params, payment_params, session_params, false) {
         Err(CliError::Core(Error::DeploySize(DeployExcessiveSizeError {
             max_deploy_size,
             actual_deploy_size,
@@ -204,6 +224,7 @@ fn should_create_transfer() {
         "1",
         deploy_params(),
         PaymentStrParams::with_amount("100"),
+        false,
     );
 
     assert!(transfer_deploy.is_ok());
@@ -222,6 +243,7 @@ fn should_create_transfer() {
         "1",
         deploy_params(),
         PaymentStrParams::with_amount("100"),
+        false,
     );
 
     assert!(transfer_deploy.is_ok());
@@ -239,6 +261,7 @@ fn should_create_transfer() {
         "1",
         deploy_params(),
         PaymentStrParams::with_amount("100"),
+        false,
     );
 
     assert!(transfer_deploy.is_ok());
@@ -257,6 +280,7 @@ fn should_fail_to_create_transfer_with_bad_args() {
         "1",
         deploy_params(),
         PaymentStrParams::with_amount("100"),
+        false,
     );
 
     println!("{:?}", transfer_deploy);
@@ -267,5 +291,80 @@ fn should_fail_to_create_transfer_with_bad_args() {
             context: "new_transfer target_account",
             error: _
         })
+    ));
+}
+
+#[test]
+fn should_create_unsigned_deploy() {
+    let deploy_params = deploy_params_without_secret_key();
+    let payment_params =
+        PaymentStrParams::with_package_hash(PKG_HASH, VERSION, ENTRYPOINT, args_simple(), "", "");
+    let session_params =
+        SessionStrParams::with_package_hash(PKG_HASH, VERSION, ENTRYPOINT, args_simple(), "", "");
+
+    let deploy =
+        deploy::with_payment_and_session(deploy_params, payment_params, session_params, true)
+            .unwrap();
+
+    assert!(deploy.approvals().is_empty());
+    assert_eq!(*deploy.account(), PublicKey::from_hex(SAMPLE_ACCOUNT).unwrap());
+}
+
+#[test]
+fn should_fail_to_create_deploy_with_no_session_account() {
+    let deploy_params = deploy_params_without_account();
+    let payment_params =
+        PaymentStrParams::with_package_hash(PKG_HASH, VERSION, ENTRYPOINT, args_simple(), "", "");
+    let session_params =
+        SessionStrParams::with_package_hash(PKG_HASH, VERSION, ENTRYPOINT, args_simple(), "", "");
+
+    let deploy =
+        deploy::with_payment_and_session(deploy_params, payment_params, session_params, true);
+    assert!(deploy.is_err());
+    assert!(matches!(
+        deploy.unwrap_err(),
+        CliError::Core(DeployBuild(DeployMissingSessionAccount))
+    ));
+}
+
+#[test]
+fn should_create_unsigned_transfer() {
+    use casper_types::{AsymmetricType, PublicKey};
+
+    // with public key.
+    let secret_key = SecretKey::generate_ed25519().unwrap();
+    let public_key = PublicKey::from(&secret_key).to_hex();
+    let transfer_deploy = deploy::new_transfer(
+        "10000",
+        None,
+        &public_key,
+        "1",
+        deploy_params_without_secret_key(),
+        PaymentStrParams::with_amount("100"),
+        true,
+    ).unwrap();
+    assert!(transfer_deploy.approvals().is_empty());
+}
+
+#[test]
+fn should_fail_to_create_transfer_without_account() {
+    use casper_types::{AsymmetricType, PublicKey};
+
+    // with public key.
+    let secret_key = SecretKey::generate_ed25519().unwrap();
+    let public_key = PublicKey::from(&secret_key).to_hex();
+    let transfer_deploy = deploy::new_transfer(
+        "10000",
+        None,
+        &public_key,
+        "1",
+        deploy_params_without_account(),
+        PaymentStrParams::with_amount("100"),
+        true,
+    );
+    assert!(transfer_deploy.is_err());
+    assert!(matches!(
+        transfer_deploy.unwrap_err(),
+        CliError::Core(DeployBuild(DeployMissingSessionAccount))
     ));
 }
