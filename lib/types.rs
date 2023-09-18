86b1baeb8ee1ba8a798897abef0bca5db5d335ba
Zach Showalter <zach@casperlabs.io>
2023-09-18 15:22:02 -0400

    Added json_block_with_signatures.rs and exported `JsonBlockWithSignatures` via types.rs for use in `GetBlockResult`
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

--- lib/types.rs
+++ lib/types.rs
@@ -3,7 +3,9 @@
 mod auction_state;
 mod deploy_execution_info;
 mod legacy_execution_result;
+mod json_block_with_signatures;
 
+pub use json_block_with_signatures::JsonBlockWithSignatures;
 pub use auction_state::AuctionState;
 pub use deploy_execution_info::DeployExecutionInfo;
 pub use legacy_execution_result::LegacyExecutionResult;
---
+++ lib/types/json_block_with_signatures.rs
@@ -0,0 +1,34 @@
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Serialize};
+use serde_map_to_array::{BTreeMapToArray, KeyValueJsonSchema, KeyValueLabels, };
+
+use casper_types::{Block, PublicKey, Signature};
+
+/// A JSON-friendly representation of a block and the signatures for that block.
+#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Debug)]
+#[cfg_attr(feature = "datasize", derive(DataSize))]
+#[serde(deny_unknown_fields)]
+pub struct JsonBlockWithSignatures {
+    /// The block.
+    pub block: Block,
+    /// The proofs of the block, i.e. a collection of validators' signatures of the block hash.
+    #[serde(with = "BTreeMapToArray::<PublicKey, Signature, BlockProofLabels>")]
+    pub proofs: BTreeMap<PublicKey, Signature>,
+}
+
+struct BlockProofLabels;
+
+impl KeyValueLabels for BlockProofLabels {
+    const KEY: &'static str = "public_key";
+    const VALUE: &'static str = "signature";
+}
+
+impl KeyValueJsonSchema for BlockProofLabels {
+    const JSON_SCHEMA_KV_NAME: Option<&'static str> = Some("BlockProof");
+    const JSON_SCHEMA_KV_DESCRIPTION: Option<&'static str> = Some(
+        "A validator's public key paired with a corresponding signature of a given block hash.",
+    );
+    const JSON_SCHEMA_KEY_DESCRIPTION: Option<&'static str> = Some("The validator's public key.");
+    const JSON_SCHEMA_VALUE_DESCRIPTION: Option<&'static str> = Some("The validator's signature.");
+}
