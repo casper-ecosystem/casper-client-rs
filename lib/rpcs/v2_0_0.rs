7b30d34d1b04426f2f7c6bb64d93f79d5f3baa40
Karan Dhareshwar <karan@casperlabs.io>
2023-10-18 10:49:46 -0500

    PR cleanup

--- lib/rpcs/v2_0_0.rs
+++ lib/rpcs/v2_0_0.rs
@@ -1,7 +1,8 @@
 //! The JSON-RPC request and response types at v2.0.0 of casper-node.
 
-pub(crate) mod get_block;
 pub(crate) mod get_deploy;
+pub(crate) mod get_block;
+
 
 // The following RPCs are all unchanged from v1.6.0, so we just re-export them.
 
--- lib/types.rs
+++ lib/types.rs
@@ -2,10 +2,11 @@
 
 mod auction_state;
 mod deploy_execution_info;
-mod json_block_with_signatures;
 mod legacy_execution_result;
+mod json_block_with_signatures;
 
+
+pub use legacy_execution_result::LegacyExecutionResult;
 pub use auction_state::AuctionState;
 pub use deploy_execution_info::DeployExecutionInfo;
 pub use json_block_with_signatures::JsonBlockWithSignatures;
-pub use legacy_execution_result::LegacyExecutionResult;
