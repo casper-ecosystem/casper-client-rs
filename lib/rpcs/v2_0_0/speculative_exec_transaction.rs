e611d6b4e340853f752c658ecf4d513304728218
Zach Showalter <zach@casperlabs.io>
2024-02-26 14:58:49 -0500

    Add parameter and result structs for the speculative exec of transactions.
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

---
+++ lib/rpcs/v2_0_0/speculative_exec_transaction.rs
@@ -0,0 +1,44 @@
+use serde::{Deserialize, Serialize};
+
+use casper_types::{
+    contract_messages::Messages, execution::ExecutionResultV2, BlockHash, ProtocolVersion,
+    Transaction,
+};
+
+use crate::rpcs::common::BlockIdentifier;
+
+pub(crate) const SPECULATIVE_EXEC_TXN_METHOD: &str = "speculative_exec_txn";
+
+/// Params for "speculative_exec_txn" RPC request.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct SpeculativeExecTxnParams {
+    /// Block hash on top of which to execute the transaction.
+    pub block_identifier: Option<BlockIdentifier>,
+    /// Transaction to execute.
+    pub transaction: Transaction,
+}
+
+impl SpeculativeExecTxnParams {
+    /// Creates a new `SpeculativeExecTxnParams`.
+    pub fn new(block_identifier: Option<BlockIdentifier>, transaction: Transaction) -> Self {
+        SpeculativeExecTxnParams {
+            block_identifier,
+            transaction,
+        }
+    }
+}
+
+/// Result for "speculative_exec_txn" and "speculative_exec" RPC responses.
+#[derive(PartialEq, Eq, Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct SpeculativeExecTxnResult {
+    /// The RPC API version.
+    pub api_version: ProtocolVersion,
+    /// Hash of the block on top of which the transaction was executed.
+    pub block_hash: BlockHash,
+    /// Result of the execution.
+    pub execution_result: ExecutionResultV2,
+    /// Messages emitted during execution.
+    pub messages: Messages,
+}
