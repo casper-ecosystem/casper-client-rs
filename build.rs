5a50ef8edd38fda57367906a2a069dabc0f7f110
Zach Showalter <zach@casperlabs.io>
2024-04-15 14:35:39 -0400

    run cargo fmt
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
-use std::{
-    process::Command
-};
+use std::process::Command;
 
 const GIT_HASH_ENV_VAR: &str = "GIT_SHA_SHORT";
 fn main() {
@@ -13,7 +11,8 @@ fn main() {
         .expect("Failed to retrieve short git commit hash");
 
     //Parse the raw output into a string, we still need to remove the newline character
-    let git_hash_raw = String::from_utf8(git_process_output.stdout).expect("Failed to convert git hash to string");
+    let git_hash_raw =
+        String::from_utf8(git_process_output.stdout).expect("Failed to convert git hash to string");
     //Remove the newline character from the short git commit hash
     let git_hash = git_hash_raw.trim_end_matches('\n');
 
