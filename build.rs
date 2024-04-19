4159153c81a6860f9a2fd463e754a40ae6b6fc22
Adam Ciarciński <aciarcinski@teonite.com>
2024-04-19 10:45:57 +0200

    Update vergen to v8. This resolves audit error.

--- build.rs
+++
@@ -1,7 +0,0 @@
-use vergen::{Config, ShaKind};
-
-fn main() {
-    let mut config = Config::default();
-    *config.git_mut().sha_kind_mut() = ShaKind::Short;
-    let _ = vergen::vergen(config);
-}
