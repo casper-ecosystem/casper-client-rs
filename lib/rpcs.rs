6b6b04d5c615e494c6d15af6edc7a4dff9fa47e5
gRoussac <gRoussac@users.noreply.github.com>
2024-05-21 12:16:44 +0200

    pub EntityOrAccount

--- lib/rpcs.rs
+++ lib/rpcs.rs
@@ -12,7 +12,9 @@ pub(crate) mod v1_6_0;
 pub(crate) mod v2_0_0;
 
 pub use v2_0_0::{
-    get_account::AccountIdentifier, get_dictionary_item::DictionaryItemIdentifier,
-    get_entity::EntityIdentifier, query_balance::PurseIdentifier,
+    get_account::AccountIdentifier,
+    get_dictionary_item::DictionaryItemIdentifier,
+    get_entity::{EntityIdentifier, EntityOrAccount},
+    query_balance::PurseIdentifier,
     query_global_state::GlobalStateIdentifier,
 };
