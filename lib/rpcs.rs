c8506167d2d312c3efd41dd806c1f529a0ccd97c
Zach Showalter <zach@casperlabs.io>
2024-05-29 15:34:33 -0400

    Add get_entity::EntityOrAccount to the list of structs exported for the clients library as structs that contain it are exported, therefore for consumers of the RPC types should have access to this type as well.
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

--- lib/rpcs.rs
+++ lib/rpcs.rs
@@ -13,6 +13,6 @@ pub(crate) mod v2_0_0;
 
 pub use v2_0_0::{
     get_account::AccountIdentifier, get_dictionary_item::DictionaryItemIdentifier,
-    get_entity::EntityIdentifier, query_balance::PurseIdentifier,
+    get_entity::{EntityIdentifier, EntityOrAccount}, query_balance::PurseIdentifier,
     query_global_state::GlobalStateIdentifier,
 };
