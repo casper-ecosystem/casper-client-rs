80da206cf61a415a8d19b3522c65e803cd6f5e3a
Zach Showalter <zach@casperlabs.io>
2024-05-30 13:39:46 -0400

    Update tests
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

--- lib/cli/tests.rs
+++ lib/cli/tests.rs
@@ -479,6 +479,7 @@ mod transaction {
     ],
     "target": "Native",
     "entry_point": "Transfer",
+    "transaction_kind": 0,
     "scheduling": "Standard"
   },
   "approvals": []
@@ -1018,7 +1019,6 @@ mod transaction {
 
         let transaction_builder_params = TransactionBuilderParams::Session {
             transaction_bytes,
-            entry_point: "entry-point-session",
         };
         let transaction =
             create_transaction(transaction_builder_params, transaction_string_params, true);
@@ -1026,7 +1026,7 @@ mod transaction {
         assert_eq!(transaction.as_ref().unwrap().chain_name(), "session");
         assert_eq!(
             transaction.as_ref().unwrap().body().entry_point(),
-            &TransactionEntryPoint::Custom("entry-point-session".to_string())
+            &TransactionEntryPoint::Call
         );
         assert_eq!(transaction.as_ref().unwrap().body().target(), target);
     }
