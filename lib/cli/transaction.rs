5962df2e1268324024ac72944f17ad5cf391d6a5
gRoussac <gRoussac@users.noreply.github.com>
2024-06-04 12:33:36 +0200

    test

--- lib/cli/transaction.rs
+++ lib/cli/transaction.rs
@@ -35,60 +35,60 @@ pub fn create_transaction(
     )?;
 
     // let timestamp = parse::timestamp(transaction_params.timestamp)?;
-    let ttl = parse::ttl(transaction_params.ttl)?;
+    //let ttl = parse::ttl(transaction_params.ttl)?;
     let maybe_session_account = parse::session_account(&transaction_params.initiator_addr)?;
 
     let mut transaction_builder = make_transaction_builder(builder_params)?;
 
     transaction_builder = transaction_builder
         //  .with_timestamp(timestamp)
-        .with_ttl(ttl)
+        // .with_ttl(ttl)
         .with_chain_name(chain_name);
 
-    if transaction_params.pricing_mode.is_empty() {
-        return Err(CliError::InvalidArgument {
-            context: "create_transaction (pricing_mode)",
-            error: "pricing_mode is required to be non empty".to_string(),
-        });
-    }
-    let pricing_mode = if transaction_params.payment_amount.to_lowercase().as_str() == "reserved" {
-        let digest = Digest::from_hex(transaction_params.receipt).map_err(|error| {
-            CliError::FailedToParseDigest {
-                context: "pricing_digest",
-                error,
-            }
-        })?;
+    // if transaction_params.pricing_mode.is_empty() {
+    //     return Err(CliError::InvalidArgument {
+    //         context: "create_transaction (pricing_mode)",
+    //         error: "pricing_mode is required to be non empty".to_string(),
+    //     });
+    // }
+    // let pricing_mode = if transaction_params.payment_amount.to_lowercase().as_str() == "reserved" {
+    //     let digest = Digest::from_hex(transaction_params.receipt).map_err(|error| {
+    //         CliError::FailedToParseDigest {
+    //             context: "pricing_digest",
+    //             error,
+    //         }
+    //     })?;
 
-        parse::pricing_mode(
-            transaction_params.pricing_mode,
-            transaction_params.payment_amount,
-            transaction_params.gas_price_tolerance,
-            transaction_params.standard_payment,
-            Some(digest),
-        )?
-    } else {
-        parse::pricing_mode(
-            transaction_params.pricing_mode,
-            transaction_params.payment_amount,
-            transaction_params.gas_price_tolerance,
-            transaction_params.standard_payment,
-            None,
-        )?
-    };
+    //     parse::pricing_mode(
+    //         transaction_params.pricing_mode,
+    //         transaction_params.payment_amount,
+    //         transaction_params.gas_price_tolerance,
+    //         transaction_params.standard_payment,
+    //         Some(digest),
+    //     )?
+    // } else {
+    //     parse::pricing_mode(
+    //         transaction_params.pricing_mode,
+    //         transaction_params.payment_amount,
+    //         transaction_params.gas_price_tolerance,
+    //         transaction_params.standard_payment,
+    //         None,
+    //     )?
+    // };
 
-    transaction_builder = transaction_builder.with_pricing_mode(pricing_mode);
+    // transaction_builder = transaction_builder.with_pricing_mode(pricing_mode);
 
-    let maybe_json_args = parse::args_json::session::parse(transaction_params.session_args_json)?;
-    let maybe_simple_args =
-        parse::arg_simple::session::parse(&transaction_params.session_args_simple)?;
+    // let maybe_json_args = parse::args_json::session::parse(transaction_params.session_args_json)?;
+    // let maybe_simple_args =
+    //     parse::arg_simple::session::parse(&transaction_params.session_args_simple)?;
 
-    let args = parse::args_from_simple_or_json(maybe_simple_args, maybe_json_args);
-    if !args.is_empty() {
-        transaction_builder = transaction_builder.with_runtime_args(args);
-    }
-    if let Some(secret_key) = &maybe_secret_key {
-        transaction_builder = transaction_builder.with_secret_key(secret_key);
-    }
+    // let args = parse::args_from_simple_or_json(maybe_simple_args, maybe_json_args);
+    // if !args.is_empty() {
+    //     transaction_builder = transaction_builder.with_runtime_args(args);
+    // }
+    // if let Some(secret_key) = &maybe_secret_key {
+    //     transaction_builder = transaction_builder.with_secret_key(secret_key);
+    // }
 
     if let Some(account) = maybe_session_account {
         transaction_builder =
